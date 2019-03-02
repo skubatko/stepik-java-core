4217a38282f26c2981e10f4afb2fb16839845e4c
Sergey Kubatko <skubatko@gmail.com>
2019-03-02 15:46:53 +0300

    feature/chapter2: case221 completed

---
+++ src/main/java/ru/skubatko/dev/java/core/stepik/chapter2/case221/Case221.java
@@ -0,0 +1,9 @@
+package ru.skubatko.dev.java.core.stepik.chapter2.case221;
+
+public class Case221 {
+
+    public static char charExpression(int a) {
+        return (char) ((int) '\\' + a);
+    }
+
+}
