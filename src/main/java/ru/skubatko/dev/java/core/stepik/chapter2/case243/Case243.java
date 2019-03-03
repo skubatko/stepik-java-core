1b75af1b9dd6058baddb49c6359676f942056592
Sergey Kubatko <skubatko@gmail.com>
2019-03-03 13:38:32 +0300

    feature/chapter2: case243 completed

--- src/main/java/ru/skubatko/dev/java/core/stepik/chapter2/case243/Case243.java
+++ src/main/java/ru/skubatko/dev/java/core/stepik/chapter2/case243/Case243.java
@@ -2,32 +2,34 @@ package ru.skubatko.dev.java.core.stepik.chapter2.case243;
 
 public class Case243 {
 
-    private String printTextPerRole(String[] roles, String[] textLines) {
-        int[] numberOfLinesInRole = new int[roles.length];
-        String[] roleTextLines = new String[roles.length];
+    public static String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder[] roleTextLines = new StringBuilder[roles.length];
+        int[] numberOfRoleTextLines = new int[roles.length];
 
         for (int lineIdx = 0; lineIdx < textLines.length; lineIdx++) {
             for (int roleIdx = 0; roleIdx < roles.length; roleIdx++) {
                 String roleToCheck = roles[roleIdx] + ": ";
                 if (textLines[lineIdx].startsWith(roleToCheck)) {
-                    numberOfLinesInRole[roleIdx]++;
 
                     int startTextLineWithNoRole = textLines[lineIdx].indexOf(": ") + 2;
                     String textLineWithNoRole = textLines[lineIdx].substring(startTextLineWithNoRole);
 
-                    roleTextLines[roleIdx] = roleTextLines[roleIdx]
-                            + numberOfLinesInRole[roleIdx]
-                            + ") "
-                            + textLineWithNoRole
-                            + "\n";
+                    int lineNumber = lineIdx + 1;
+                    if (roleTextLines[roleIdx] == null)
+                        roleTextLines[roleIdx] = new StringBuilder(roles[roleIdx]).append(":\n");
+                    roleTextLines[roleIdx].append(lineNumber).append(") ").append(textLineWithNoRole).append("\n");
+
+                    numberOfRoleTextLines[roleIdx]++;
                     break;
                 }
             }
         }
 
         StringBuilder result = new StringBuilder();
-        for (String roleTextLine : roleTextLines)
+        for (StringBuilder roleTextLine : roleTextLines)
             result.append(roleTextLine).append("\n");
+
+        result.deleteCharAt(result.length() - 1);
 
         return result.toString();
     }
