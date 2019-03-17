037ce2dbc0e1551f1508203d8b886174b57b7f3f
Sergey Kubatko <skubatko@gmail.com>
2019-03-17 14:07:44 +0300

    chapter4: case341v3 updated

---
+++ src/main/java/ru/skubatko/dev/java/core/stepik/chapter3/case341/ComplexNumberV3.java
@@ -0,0 +1,56 @@
+package ru.skubatko.dev.java.core.stepik.chapter3.case341;
+
+public final class ComplexNumberV3 {
+    private final double re;
+    private final double im;
+
+    public ComplexNumberV3(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+
+        ComplexNumberV3 complexNumberV2 = (ComplexNumberV3) o;
+
+        return (Double.compare(re, complexNumberV2.re) == 0)
+                && (Double.compare(im, complexNumberV2.im) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        long l = 0L;
+
+        l = Double.doubleToLongBits(re);
+        result += 31 * result + (int) (l ^ (l >>> 32));
+
+        l = Double.doubleToLongBits(im);
+        result += 31 * result + (int) (l ^ (l >>> 32));
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "re=" + re + ", im=" + im;
+    }
+}
