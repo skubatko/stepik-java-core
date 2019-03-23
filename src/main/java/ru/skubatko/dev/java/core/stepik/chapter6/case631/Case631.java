05c90056689e51bb93bd7ca172e946d66c4b686a
Sergey Kubatko <skubatko@gmail.com>
2019-03-23 15:58:52 +0300

    chapter6: case631 completed

---
+++ src/main/java/ru/skubatko/dev/java/core/stepik/chapter6/case631/Case631.java
@@ -0,0 +1,25 @@
+package ru.skubatko.dev.java.core.stepik.chapter6.case631;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Case631 {
+    public static void main(String[] args) {
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+        System.out.println(safeStringLength.apply("test"));
+        System.out.println(safeStringLength.apply(null));
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
+    }
+
+}
